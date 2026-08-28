a92b2f99db86111a8c02d9742535fdb69f39e840
milan <milan.duettra@gmail.com>
2026-08-28 20:41:39 +0200

    Official V4.0.1 Release Release Notes: - Fixed mayor bugs in the script.js file.
    All Changes:
    	modified:   README.md
    	modified:   assets/css/main.css
    	modified:   assets/script/script.js
    	modified:   index.html
    
    If you find any issues, please report them on the GitHub Issues page or on Discord DM under the following link:
    https://discord.com/users/1215682123219079259
    
    You can find the preceding version of this project at the following link:
    https://github.com/m1l4n10/linktree/commit/4fc94f02a593d32e15277751393a50bedf124ebc
    
    You can find the latest version of this project at the following link:
    https://github.com/m1l4n10/linktree

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,7 @@ document.body.style.backgroundAttachment = "fixed";
 document.body.style.backgroundPosition = "center";
 document.body.style.backgroundRepeat = "no-repeat";
 
-fetch('data/URL.JSON')
+fetch('../../data/URL.JSON')
   .then(response => {
     if (!response.ok) {
       throw new Error('JSON konnte nicht geladen werden');
